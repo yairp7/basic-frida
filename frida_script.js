3ca6d4a7b6f3cee17af08b8052d59a307f14634d
yair.pecherer <yair.pecherer@similarweb.com>
2018-12-09 22:59:58 +0200

    Small changes

--- frida_script.js
+++ frida_script.js
@@ -1,7 +1,23 @@
-function monitorFunction(package_name, class_name, func_name) {
+/**
+    This function is used to create a hook on the method described in: 
+    package_name.class_name.func_name(func_args)
+
+    The hook will not modify the functionality of the method but only 
+    print out the provided parameters and the result returned every time
+    the method is called.
+
+    Arguments:
+    package_name - The package name of the class the method is in
+    class_name - The class name of the class the method is in
+    func_name - The name of the method we want to monitor
+    func_args - A list of arguments of the specific method, or undefined
+                in case we want to hook all available overrides of the method
+*/
+function monitorFunction(package_name, class_name, func_name, func_args) {
     try {
         const full_class_name = package_name + '.' + class_name;
         const cls = Java.use(full_class_name);
+        var should_hook = true;
         var overloads = cls[func_name].overloads;
         for (var index in overloads) {
             var method_overload = overloads[index];
@@ -12,18 +28,40 @@ function monitorFunction(package_name, class_name, func_name) {
                 for (j in method_overload.argumentTypes) {
                     args_types.push(method_overload.argumentTypes[j].className);
                 }
-                send(msg + '(' + args_types.toString() + ')\n');
-                try {
-                    method_overload.implementation = function () {
-                        var args = [].slice.call(arguments);
-                        var result = this[func_name].apply(this, args);
-                        var msg = func_name + '(' + args.join(', ') + ') => ' + result + '\n';
-                        send(msg);
-                        return result;
+
+                // Check if we are looking for a specific overload
+                if(func_args != undefined) {
+                    should_hook = false;
+                    if(method_overload.argumentTypes.length == func_args.length) {
+                        var num_of_same_args = 0;
+                        for (var a in method_overload.argumentTypes) {
+                            if(method_overload.argumentTypes[a] == func_args[a]) {
+                                num_of_same_args++;
+                            }
+                            else {
+                                break;
+                            }
+                        }
+                        if(num_of_same_args = func_args.length) {
+                            should_hook = true;
+                        }
                     }
                 }
-                catch(e) { 
-                    send("ERROR: " + e); 
+
+                if(should_hook) {
+                    send(msg + '(' + args_types.toString() + ')\n');
+                    try {
+                        method_overload.implementation = function () {
+                            var args = [].slice.call(arguments);
+                            var result = this[func_name].apply(this, args);
+                            var msg = func_name + '(' + args.join(', ') + ') => ' + result + '\n';
+                            send(msg);
+                            return result;
+                        }
+                    }
+                    catch(e) { 
+                        send("ERROR: " + e); 
+                    }
                 }
             }
         }
@@ -33,6 +71,19 @@ function monitorFunction(package_name, class_name, func_name) {
     }
 }
 
+/**
+    This function is used to create a hook on all the methods described in
+    the following class: 
+    package_name.class_name
+
+    The hook will not modify the functionality of the methods but only 
+    print out the provided parameters and the result returned every time
+    a method is called.
+
+    Arguments:
+    package_name - The package name of the class the method is in
+    class_name - The class name of the class the method is in
+*/
 function monitorClass(package_name, class_name) {
     var full_class_name = package_name + '.' + class_name;
     const cls = Java.use(full_class_name);
@@ -51,7 +102,7 @@ function monitorClass(package_name, class_name) {
 
 if (Java.available) {
     Java.perform(function() {
-        monitorFunction('org.json', 'JSONObject', 'toString');
-        monitorClass('com.company.activities', 'MainActivity');
+        // monitorFunction('<package-name>', '<class-name>', '<method-name>');
+        // monitorClass('<package-name>', '<class-name>');
     });
 }
---
+++ wrapper.py
@@ -0,0 +1,33 @@
+import frida
+import sys
+
+# Provide the application’s package name
+app = sys.argv[1] 
+
+def message_callback(message):
+	print message
+
+# Read our code file
+frida_code = None
+with open('frida_code.js') as f: 
+	frida_code = f.read()
+
+if frida_code:
+	# Get all devices connected
+	devices = frida.get_device_manager().enumerate_devices()
+	if devices and len(devices) > 0:
+		usb_devices = []
+		for device in devices: # Iterate through the devices
+			if device.type == 'usb':
+				usb_devices.append(device)
+			if usb_devices and len(usb_devices) > 0:
+				for device in usb_devices:
+					pid = device.get_process(app).pid # Get the process id
+					process = device.attach(pid) # Attach frida to the process
+					# Create a script object from the provided code
+					script = process.create_script(frida_code)
+					# Create a javascript event listener to get callbacks from the 
+					# javascript code to the python frida wrapper
+					script.on('message', message_callback) 
+					# Load the script into frida
+			        script.load()
